c2e9df09d3adc8a293b45766441566bffe19fd08
Airlangga Permana Putra <airlanggapermana96@gmail.com>
2020-11-22 03:48:20 +0700

    Update Task2.py

--- Week 2 PT[2]/Task2.py
+++ Week 2 PT[2]/Task2.py
@@ -1,20 +1,22 @@
 klub_a = input('Klub A : ')
 klub_b = input('Klub B : ')
 arData = []
-for i in range (999999):
-    print(f'Pertandingan Ke-{i+1} :', end=' ')
-    skor_a,skor_b = input().split()
-    convert_a = int(skor_a)
-    convert_b = int(skor_b)
+kondisi = True
+while kondisi:
+    for i in range (9999):
+        print(f'Pertandingan Ke-{i+1} :', end=' ')
+        skor_a,skor_b = input().split()
+        convert_a = int(skor_a)
+        convert_b = int(skor_b)
 
-    if convert_a > convert_b:
-        arData.append(klub_a)
-    elif convert_a < convert_b:
-        arData.append(klub_b)
-    
-    if (convert_a | convert_b < 0):
-        for i in range (i):
-            hasil = arData[i]
-            print(f'Hasil {i+1} : ',hasil)
-            
-        break    
+        if convert_a > convert_b:
+            arData.append(klub_a)
+        elif convert_a < convert_b:
+            arData.append(klub_b)
+        
+        if (convert_a | convert_b < 0):
+            for i in range (i):
+                hasil = arData[i]
+                print(f'Hasil {i+1} : ',hasil)
+            kondisi = False    
+            break    
